747ae82cca1810d9085ce31de1584eb3c9ffa455
MrRud <mrrudrud@gmail.com>
2018-01-15 15:19:01 +0700

    064-building-a-sticky-navigation-part-2

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,8 +1,19 @@
-
 $(document).ready(function() {
-   
-    $('h1').click(function(){
-       $(this).css('background-color', '#ff0000'); 
-    });
     
+    var waypoint = new Waypoint({
+        element: document.getElementById('js--section-features'),
+        handler: function(direction) {
+            if(direction == "down"){
+                $('nav').addClass('sticky');
+            } else {
+                $('nav').removeClass('sticky');
+            }
+        },
+        offset: '15%'
+    });
+//   
+//    $('h1').click(function(){
+//       $(this).css('background-color', '#ff0000'); 
+//    });
+//    
 });
