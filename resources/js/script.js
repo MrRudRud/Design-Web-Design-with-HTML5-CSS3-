e83863433d76356ae01816d710cb07e91b3ea22d
MrRud <mrrudrud@gmail.com>
2018-01-16 15:19:12 +0700

    064-building-a-sticky-navigation-part-2

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    
     var waypoint = new Waypoint({
         element: document.getElementById('js--section-features'),
         handler: function(direction) {
@@ -9,11 +8,46 @@ $(document).ready(function() {
                 $('nav').removeClass('sticky');
             }
         },
-        offset: '15%'
+        offset: '10%'
     });
+    
+
+    //Scroll on Buttons
+    $('.js--scroll-to-plan').click(function () {
+        $('html, body').animate({scrollTop: $('#js--section-plans').offset().top}, 1000)
+    })    
+    
+    //#js-section-features -->by ID
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('#js--section-features').offset().top}, 1000)
+    })
+
+});
+    
+//    var waypoints = $('.js--section-features').waypoint({
+//        handler: function(direction) {
+//            if(direction == "down"){
+//                $('nav').addClass('sticky');
+//            } else {
+//                $('nav').removeClass('sticky');
+//            }
+//        },
+//        offset: '15%'
+//    });
+    
+//    $('.js--section-features').Waypoint(function(direction){
+//        if(direction == "down"){
+//            $('nav').addClass('sticky');
+//        } else {
+//            $('nav').removeClass('sticky');
+//        }
+//    },{ offset: '15%;' 
+//        
+//    });
+    
+
 //   
 //    $('h1').click(function(){
 //       $(this).css('background-color', '#ff0000'); 
 //    });
 //    
-});
